6846ba732db9e63437a7930be99a01c4e6ea58a0
td-dana <40476570+td-dana@users.noreply.github.com>
2018-07-18 12:09:48 -0700

    commit all portfolio files

---
+++ main.js
@@ -0,0 +1,21 @@
+alert("Hey! This is an alert box");
+
+//var myButton = document.querySelector("button");
+//var myHeading = document.querySelector("h1");
+
+//myButton.onclick = function () {
+  //var name = prompt("Please enter your name");
+  //localStorage.setItem ("name: ", name);
+  // document.getElementById("mybutton").innerHTML = localStorage.getItem("name: ");
+  //document.getElementById("mybutton").innerHTML = "asdlkfjklsadj"
+
+
+//   myHeading.innerHTML() = name;
+}
+
+// create an if/else to check if the there is a storedName
+// hint use local Storage.getItem
+
+// myButton.onclick = function () {
+  // call the setUserName function here
+}
